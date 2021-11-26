f92d6c0bb74a4bbf992bb64134fec6200a5080e6
Larissa Novais <larinovais54@gmail.com>
2021-11-26 20:52:56 -0300

    Modelar classe cliente

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/Cliente.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/Cliente.java
@@ -1,5 +1,26 @@
 package br.com.zup.MiniProjetoModulo05Elegance.cliente;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "clientes")
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Cliente {
+    @Id
+    private String cpf;
+    private String nome;
+    @Column(unique = true)
+    private String email;
+    private String telefone;
+    private String endereco;
 }
