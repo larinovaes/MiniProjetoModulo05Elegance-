2b0869d13b922f29de370b6848331b18f35f90b3
Larissa Novais <larinovais54@gmail.com>
2021-11-26 12:12:16 -0300

    Inicio do projeto

---
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/MiniProjetoModulo05EleganceApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.MiniProjetoModulo05Elegance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniProjetoModulo05EleganceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniProjetoModulo05EleganceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/zup/MiniProjetoModulo05Elegance/MiniProjetoModulo05EleganceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.MiniProjetoModulo05Elegance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniProjetoModulo05EleganceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
