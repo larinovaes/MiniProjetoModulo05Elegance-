f92b17b1aa2907ae9c2804ace4c861374e4b2479
Larissa Novais <larinovais54@gmail.com>
2021-11-26 20:39:55 -0300

    Modelar classe produto

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/Produto.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/Produto.java
@@ -1,4 +1,23 @@
 package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "produtos")
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Produto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer codigoDoProduto;
+    private String nomeDoProduto;
+    private Double valorDoProduto;
+    private Integer quantidadeDeProduto;
+
 }
