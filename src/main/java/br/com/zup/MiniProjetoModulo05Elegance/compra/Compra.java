585e4249ca9ab4f9d047b81fb4423649c06a31ba
Larissa Novais <larinovais54@gmail.com>
2021-11-26 21:05:23 -0300

    Modelar classe de compras

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
@@ -1,5 +1,21 @@
 package br.com.zup.MiniProjetoModulo05Elegance.compra;
 
+import br.com.zup.MiniProjetoModulo05Elegance.cliente.Cliente;
+import br.com.zup.MiniProjetoModulo05Elegance.produto.Produto;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+import java.util.List;
+
 public class Compra {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer numeroDoPedido;
+    @ManyToOne
+    private Cliente cliente;
+    @OneToMany
+    private List<Produto> produtos;
+    private Double valor;
+    private LocalDateTime dataDeCompra;
 
 }
