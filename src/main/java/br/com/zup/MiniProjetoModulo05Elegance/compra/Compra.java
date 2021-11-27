dfb22d91b3a912f0762d2bc0fb9e6e73f6badd9a
Larissa Novais <larinovais54@gmail.com>
2021-11-26 21:07:17 -0300

    Adicionar anotações

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
@@ -2,11 +2,20 @@ package br.com.zup.MiniProjetoModulo05Elegance.compra;
 
 import br.com.zup.MiniProjetoModulo05Elegance.cliente.Cliente;
 import br.com.zup.MiniProjetoModulo05Elegance.produto.Produto;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Entity
+@Table(name = "compras")
+
+@NoArgsConstructor
+@Getter
+@Setter
 public class Compra {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
