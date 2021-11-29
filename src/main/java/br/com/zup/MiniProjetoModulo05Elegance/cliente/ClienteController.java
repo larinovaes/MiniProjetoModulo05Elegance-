ff9e776ce5ab0701fcdf561e5d683aca4d2dae9c
Larissa Novais <larinovais54@gmail.com>
2021-11-29 10:24:17 -0300

    Criar classe cliente controller

---
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteController.java
@@ -0,0 +1,10 @@
+package br.com.zup.MiniProjetoModulo05Elegance.cliente;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/clientes")
+public class ClienteController {
+
+}
