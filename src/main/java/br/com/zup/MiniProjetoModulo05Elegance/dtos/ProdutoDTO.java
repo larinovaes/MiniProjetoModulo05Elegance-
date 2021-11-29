7f6ab300dcfd985ecc75fdea2e40aabf2b0ad463
Larissa Novais <larinovais54@gmail.com>
2021-11-29 12:24:05 -0300

    Adicionar Validações

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/ProdutoDTO.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/ProdutoDTO.java
@@ -1,14 +1,20 @@
 package br.com.zup.MiniProjetoModulo05Elegance.dtos;
 
+import com.sun.istack.NotNull;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
 
 @NoArgsConstructor
 @Getter
 @Setter
 public class ProdutoDTO {
+    @NotBlank
     private String nomeDoProduto;
+    @NotNull
     private Double valorDoProduto;
+    @NotNull
     private Integer quantidadeDeProduto;
 }
