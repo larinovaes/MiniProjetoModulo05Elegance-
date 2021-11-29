65b42727bee4c85171e728ca6b81465368cb45c1
Larissa Novais <larinovais54@gmail.com>
2021-11-29 10:39:19 -0300

    Criar classe produto service

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
@@ -1,2 +1,9 @@
-package br.com.zup.MiniProjetoModulo05Elegance.produto;public class ProdutoController {
+package br.com.zup.MiniProjetoModulo05Elegance.produto;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/produtos")
+public class ProdutoController {
 }
