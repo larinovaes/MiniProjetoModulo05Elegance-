60b8d3369b68f81363d683c0696f7f8a521849fe
Larissa Novais <larinovais54@gmail.com>
2021-11-29 10:30:27 -0300

    Criar interface repository

---
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraRepository.java
@@ -0,0 +1,6 @@
+package br.com.zup.MiniProjetoModulo05Elegance.compra;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface CompraRepository extends CrudRepository<Compra, Integer> {
+}
