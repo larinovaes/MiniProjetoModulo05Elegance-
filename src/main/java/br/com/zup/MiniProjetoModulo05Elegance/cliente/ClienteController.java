850f29806ff97a83dab072895fe99bd1f417ee19
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-29 14:41:32 -0300

    Adicionar metodo cadastrarCliente na Classe ClienteController

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteController.java
@@ -1,10 +1,20 @@
 package br.com.zup.MiniProjetoModulo05Elegance.cliente;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clientes")
 public class ClienteController {
+    @Autowired
+    private ClienteService clienteService;
+
+    @PostMapping
+    public Cliente cadastrarCliente(@RequestBody Cliente cliente) {
+        return clienteService.salvarCliente(cliente);
+    }
 
 }
