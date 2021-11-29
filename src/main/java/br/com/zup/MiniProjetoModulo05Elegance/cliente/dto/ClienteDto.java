2ed9cb35d37516b602db33999506d45fabb2ae17
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-29 12:13:19 -0300

    Adicionar getters, setters, metodos construtor

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/dto/ClienteDto.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/dto/ClienteDto.java
@@ -1,5 +1,18 @@
 package br.com.zup.MiniProjetoModulo05Elegance.cliente.dto;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@Getter
+@Setter
+
 public class ClienteDto {
+    private String cpf;
+    private String nome;
+    private String email;
+    private String telefone;
+    private String endereco;
 
 }
