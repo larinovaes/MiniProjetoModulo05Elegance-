772b3a26e8ef2e5185fbf56cf40c30af6569d977
Larissa Novais <larinovais54@gmail.com>
2021-11-29 10:54:27 -0300

    Criar classe compra controller

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraController.java
@@ -1,2 +1,9 @@
-package br.com.zup.MiniProjetoModulo05Elegance.compra;public class CompraController {
+package br.com.zup.MiniProjetoModulo05Elegance.compra;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/compras")
+public class CompraController {
 }
