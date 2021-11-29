a83d696321f35af6266be9fa776295fd71c85ac6
Larissa Novais <larinovais54@gmail.com>
2021-11-29 12:18:00 -0300

    Criar metodo para salvar produtos

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
@@ -1,9 +1,20 @@
 package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
+    @Autowired
+    private ProdutoService produtoService;
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Produto salvarProduto(@RequestBody Produto produto) {
+        produtoService.cadastoProduto(produto);
+        return produto;
+    }
 }
