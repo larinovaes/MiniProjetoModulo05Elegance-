01729814ae8d9811e9c6cd7eabf5600c39c895a4
Larissa Novais <larinovais54@gmail.com>
2021-11-29 13:23:47 -0300

    Adicionar metodo de model mapper

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/mapper/Modelmapper.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/mapper/Modelmapper.java
@@ -1,6 +1,14 @@
 package br.com.zup.MiniProjetoModulo05Elegance.mapper;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.context.annotation.Bean;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Modelmapper {
 
-
+    @Bean
+    public ModelMapper modelMapper() {
+        return modelMapper();
+    }
 }
