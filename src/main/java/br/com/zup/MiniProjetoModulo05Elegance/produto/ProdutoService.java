1716a552a588c59678003e9106fd9b25a436a482
Larissa Novais <larinovais54@gmail.com>
2021-11-29 12:03:39 -0300

    Criar metodo para cadastrar produto

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoService.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoService.java
@@ -1,7 +1,16 @@
 package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ProdutoService {
+
+    @Autowired
+    private ProdutoRepository produtoRepository;
+
+    public Produto cadastoProduto(Produto produto) {
+        produtoRepository.save(produto);
+        return produto;
+    }
 }
