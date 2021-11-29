a901f30a5a1f5a30ea0eb3111de41ec5361570e6
Larissa Novais <larinovais54@gmail.com>
2021-11-29 10:21:43 -0300

    Criar interface repository cliente

---
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteRepositoy.java
@@ -0,0 +1,6 @@
+package br.com.zup.MiniProjetoModulo05Elegance.cliente;
+
+import org.springframework.data.repository.CrudRepository;
+
+public interface ClienteRepositoy extends CrudRepository<Cliente, String> {
+}
