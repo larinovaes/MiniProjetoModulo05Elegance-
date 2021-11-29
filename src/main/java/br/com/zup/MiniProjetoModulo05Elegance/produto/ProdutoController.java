4d193d3cfc0b27a0901c3f64ed40e88ea79d1eb2
Larissa Novais <larinovais54@gmail.com>
2021-11-29 14:07:40 -0300

    Converter Produto para ProdutoDTO

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
@@ -1,6 +1,8 @@
 package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
 
+import br.com.zup.MiniProjetoModulo05Elegance.dtos.ProdutoDTO;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -13,10 +15,16 @@ public class ProdutoController {
     @Autowired
     private ProdutoService produtoService;
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public Produto salvarProduto(@RequestBody @Valid Produto produto) {
+    public ProdutoDTO salvarProduto(@RequestBody @Valid ProdutoDTO produtoDTO) {
+        Produto produto = modelMapper.map(produtoDTO, Produto.class);
         produtoService.cadastoProduto(produto);
-        return produto;
+        produtoDTO = modelMapper.map(produto, ProdutoDTO.class);
+        return produtoDTO;
+
     }
 }
