7a480ecabcc6c1d59f767e10fafb07d96043d40a
Larissa Novais <larinovais54@gmail.com>
2021-11-30 16:28:12 -0300

    Criar filtro de produto

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoRepository.java
@@ -2,6 +2,9 @@ package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
 
+    List<Produto> findAllByCodigoDoProduto(Integer codigoDoProduto);
 }
