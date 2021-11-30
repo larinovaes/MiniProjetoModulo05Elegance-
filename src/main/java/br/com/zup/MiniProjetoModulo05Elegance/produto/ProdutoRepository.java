3b75fedfb867a41b3cd45f65636e2f144f0ef0e3
Larissa Novais <larinovais54@gmail.com>
2021-11-30 12:20:42 -0300

    Ajustes

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoRepository.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoRepository.java
@@ -3,4 +3,5 @@ package br.com.zup.MiniProjetoModulo05Elegance.produto;
 import org.springframework.data.repository.CrudRepository;
 
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
+
 }
