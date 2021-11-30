4ab61853605ac60828d37b5f6d2bd00b6c25b21d
Larissa Novais <larinovais54@gmail.com>
2021-11-30 12:00:09 -0300

    Criar metodo na controller para mapear lista de produtos

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/produto/ProdutoController.java
@@ -2,14 +2,13 @@ package br.com.zup.MiniProjetoModulo05Elegance.produto;
 
 
 import br.com.zup.MiniProjetoModulo05Elegance.dtos.ProdutoDTO;
-import br.com.zup.MiniProjetoModulo05Elegance.produto.Produto;
-import br.com.zup.MiniProjetoModulo05Elegance.produto.ProdutoService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
@@ -26,7 +25,12 @@ public class ProdutoController {
         Produto produto = modelMapper.map(produtoDTO, Produto.class);
         produtoService.cadastoProduto(produto);
         produtoDTO = modelMapper.map(produto, ProdutoDTO.class);
-        return produtoDTO;
 
+        return produtoDTO;
+    }
+
+    @GetMapping
+    public List<Produto> exibirListaDeProdutos(Produto produto) {
+        return produtoService.exibirListaDeProdutos(produto);
     }
 }
