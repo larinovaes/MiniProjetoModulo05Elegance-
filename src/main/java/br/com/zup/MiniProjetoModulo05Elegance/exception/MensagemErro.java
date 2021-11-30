1a2b00b0bf8a843e249654cfcbe5c23a5470b6b3
Larissa Novais <larinovais54@gmail.com>
2021-11-30 10:13:32 -0300

    Retirar atributo de campo

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/exception/MensagemErro.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/exception/MensagemErro.java
@@ -9,12 +9,6 @@ import lombok.Setter;
 @Setter
 public class MensagemErro {
     private String mensagem;
-    private String campo;
-
-    public MensagemErro(String mensagem, String campo) {
-        this.mensagem = mensagem;
-        this.campo = campo;
-    }
 
     public MensagemErro(String mensagem) {
         this.mensagem = mensagem;
