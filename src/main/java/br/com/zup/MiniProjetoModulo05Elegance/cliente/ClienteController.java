8a55f391ca38bec407e8da7ac6470907ffece9a6
Karen Almeida <karenalmeida61@hotmail.com>
2021-11-30 12:29:32 -0300

    Adicionar, na Classe ClienteController, metodo para exibir todos os clientes

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteController.java
@@ -1,12 +1,15 @@
 package br.com.zup.MiniProjetoModulo05Elegance.cliente;
 
 import br.com.zup.MiniProjetoModulo05Elegance.dtos.ClienteDto;
+import br.com.zup.MiniProjetoModulo05Elegance.dtos.ClienteFiltroDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 
 @RestController
@@ -26,5 +29,17 @@ public class ClienteController {
     }
 }
 
+<<<<<<< HEAD
+=======
+    @GetMapping
+    public List<ClienteFiltroDTO> exibirClientes() {
+        List<ClienteFiltroDTO> listaDeClientes = new ArrayList<>();
+        for (Cliente clienteReferencia : clienteService.exibirCliente()) {
+            ClienteFiltroDTO clienteFiltro = modelMapper.map(clienteReferencia, ClienteFiltroDTO.class);
+            listaDeClientes.add(clienteFiltro);
+        }
+        return listaDeClientes;
+    }
+>>>>>>> f5e5ff0 (Adicionar, na Classe ClienteController, metodo para exibir todos os clientes)
 
 
