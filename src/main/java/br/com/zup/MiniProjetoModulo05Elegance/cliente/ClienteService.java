34439b01f8a545287693919a6b269421630104d7
Raline Silva <ralinesilva1@gmail.com>
2021-11-30 09:04:26 -0300

    verificar email repetido

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteService.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/cliente/ClienteService.java
@@ -10,6 +10,16 @@ public class ClienteService {
     private ClienteRepositoy clienteRepositoy;
 
     public Cliente salvarCliente(Cliente cliente) {
+        Cliente cliente1 = pesquisarEmailRepetido(cliente);
+        return clienteRepositoy.save(cliente);
+    }
+
+    public Cliente pesquisarEmailRepetido(Cliente cliente) {
+        for (Cliente clienteReferencia : clienteRepositoy.findAll()) {
+            if (clienteReferencia.getEmail().equalsIgnoreCase(cliente.getEmail())) {
+                throw new RuntimeException("Esse email ja possui cadastro em nosso banco!");
+            }
+        }
         return clienteRepositoy.save(cliente);
     }
 
