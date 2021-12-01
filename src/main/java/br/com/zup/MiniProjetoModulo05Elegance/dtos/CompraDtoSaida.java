086cfe459232caa6a6e68c94dfdef0e5639e4881
Karen Almeida <karenalmeida61@hotmail.com>
2021-12-01 16:49:02 -0300

    Modelar Classe CompraDtosaida

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/CompraDtoSaida.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/CompraDtoSaida.java
@@ -1,4 +1,15 @@
 package br.com.zup.MiniProjetoModulo05Elegance.dtos;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
 public class CompraDtoSaida {
+    private String cpf;
+    private String nome;
+    private String telefone;
 }
