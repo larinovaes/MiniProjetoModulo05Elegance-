c659095efa67dd4fd51d3849b78e20a5f7f6bb07
Karen Almeida <karenalmeida61@hotmail.com>
2021-12-01 17:58:40 -0300

    Adicionar getters, setters e metodo construtor

---
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/AdicionarCompraDTO.java
@@ -0,0 +1,2 @@
+package br.com.zup.MiniProjetoModulo05Elegance.dtos;public class AdicionarCompraDTO {
+}
--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/ProdutoDTO.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/ProdutoDTO.java
@@ -11,6 +11,7 @@ import javax.validation.constraints.Size;
 @NoArgsConstructor
 @Getter
 @Setter
+
 public class ProdutoDTO {
     private Integer codigoDoProduto;
     @NotBlank
