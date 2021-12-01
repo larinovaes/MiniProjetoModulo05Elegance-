91771d6233981694ab1669098dc05dc559d937da
Karen Almeida <karenalmeida61@hotmail.com>
2021-12-01 18:03:02 -0300

    Criar Classe AdicionarCompraDto

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/AdicionarCompraDTO.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/dtos/AdicionarCompraDTO.java
@@ -1,2 +1,14 @@
-package br.com.zup.MiniProjetoModulo05Elegance.dtos;public class AdicionarCompraDTO {
+package br.com.zup.MiniProjetoModulo05Elegance.dtos;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+
+public class AdicionarCompraDTO {
+    private String cpf;
+    private Integer numeroDoPedido;
 }
