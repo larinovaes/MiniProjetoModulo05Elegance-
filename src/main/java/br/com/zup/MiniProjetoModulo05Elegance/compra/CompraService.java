7a8c4d54ec01df06c12b94385fded497168d4d25
Karen Almeida <karenalmeida61@hotmail.com>
2021-12-01 13:42:42 -0300

    Adicionar método salvarCompra

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraService.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraService.java
@@ -1,19 +1,52 @@
 package br.com.zup.MiniProjetoModulo05Elegance.compra;
 
+import br.com.zup.MiniProjetoModulo05Elegance.cliente.Cliente;
+import br.com.zup.MiniProjetoModulo05Elegance.cliente.ClienteRepositoy;
+import br.com.zup.MiniProjetoModulo05Elegance.cliente.ClienteService;
+import br.com.zup.MiniProjetoModulo05Elegance.produto.Produto;
 import br.com.zup.MiniProjetoModulo05Elegance.produto.ProdutoRepository;
+import br.com.zup.MiniProjetoModulo05Elegance.produto.ProdutoService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Service
 public class CompraService {
 
     private CompraRepository compraRepository;
+
     private ProdutoRepository produtoRepository;
+
+    @Autowired
+    private ClienteRepositoy clienteRepositoy;
+    @Autowired
+    private ClienteService clienteService;
+    @Autowired
+    private ProdutoService produtoService;
+
 
     @Autowired
     public CompraService(CompraRepository compraRepository, ProdutoRepository produtoRepository) {
         this.compraRepository = compraRepository;
         this.produtoRepository = produtoRepository;
+
+    }
+
+    public Compra salvarCompra(Compra compra) {
+        Cliente clientes = clienteService.buscarClienteporCpf(compra.getCliente().getCpf());
+        compra.setCliente((Cliente) clientes);
+
+        List<Produto> produtos = buscarProdutos(compra.getProdutos());
+        compra.setProdutos(produtos);
+        return compraRepository.save(compra);
     }
 }
+
+
+
+
+
+
