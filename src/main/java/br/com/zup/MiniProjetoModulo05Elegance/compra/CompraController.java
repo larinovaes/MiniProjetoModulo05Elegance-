4fc81a7e734985bd3d47f5efff446d4a1f5b4724
Larissa Novais <larinovais54@gmail.com>
2021-12-02 11:51:28 -0300

    Criar metodo para mapear compra especifica

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraController.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/CompraController.java
@@ -1,12 +1,17 @@
 package br.com.zup.MiniProjetoModulo05Elegance.compra;
 
+import br.com.zup.MiniProjetoModulo05Elegance.cliente.Cliente;
 import br.com.zup.MiniProjetoModulo05Elegance.dtos.AdicionarCompraDTO;
+import br.com.zup.MiniProjetoModulo05Elegance.dtos.ClienteFiltroDTO;
 import br.com.zup.MiniProjetoModulo05Elegance.dtos.CompraDTO;
 import br.com.zup.MiniProjetoModulo05Elegance.dtos.CompraSaidaDTO;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/compras")
@@ -30,5 +35,20 @@ public class CompraController {
     @ResponseStatus(HttpStatus.CREATED)
     public void adicionarCompraAoCliente(@RequestBody AdicionarCompraDTO adicionarCompraDTO) {
         compraService.adicionarCompraAoCliente(adicionarCompraDTO.getCpf(), adicionarCompraDTO.getNumeroDoPedido());
+    }
+    @GetMapping
+    public List<CompraSaidaDTO> exibirListaDeCompras() {
+        List<CompraSaidaDTO> listarCompras = new ArrayList<>();
+        for (Compra compra : compraService.listarCompras()) {
+            CompraSaidaDTO compraSaidaDTO = modelMapper.map(compra, CompraSaidaDTO.class);
+            listarCompras.add(compraSaidaDTO);
+        }
+        return listarCompras;
+    }
+
+    @GetMapping("/{numeroDoPedido}")
+    public CompraSaidaDTO exibirCompraEspecifica(@PathVariable Integer numeroDoPedido) {
+        Compra compra = compraService.exibirCompraEspecifica(numeroDoPedido);
+        return modelMapper.map(compra, CompraSaidaDTO.class);
     }
 }
