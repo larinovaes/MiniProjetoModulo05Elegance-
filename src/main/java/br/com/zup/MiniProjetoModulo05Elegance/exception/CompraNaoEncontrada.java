84f6f3451fbb37e945761c798486535e819ed361
Larissa Novais <larinovais54@gmail.com>
2021-12-02 12:11:47 -0300

    Criar classe para excessão

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/exception/CompraNaoEncontrada.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/exception/CompraNaoEncontrada.java
@@ -1,2 +1,7 @@
-package br.com.zup.MiniProjetoModulo05Elegance.exception;public class CompraNaoEncontrada {
+package br.com.zup.MiniProjetoModulo05Elegance.exception;
+
+public class CompraNaoEncontrada extends RuntimeException{
+    public CompraNaoEncontrada(String message) {
+        super(message);
+    }
 }
