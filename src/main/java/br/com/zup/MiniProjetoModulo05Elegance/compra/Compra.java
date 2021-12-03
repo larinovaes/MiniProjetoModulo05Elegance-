195aca8d56ceba6242e42065e70ebc246dd7726b
Larissa Novais <larinovais54@gmail.com>
2021-12-03 11:08:27 -0300

    Retirar importação

--- src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
+++ src/main/java/br/com/zup/MiniProjetoModulo05Elegance/compra/Compra.java
@@ -7,9 +7,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.Date;
 import java.util.List;
 
 @Entity
